1ef7e6597a2ddc4b6abae4f2b68d2a1a6a809c91
smhasanjamil <smhasanjamil23@gmail.com>
2023-06-04 14:27:08 +0600

    Banner added

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
 
 
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
