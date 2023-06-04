8968ce2cb80cdd066256d649339ef38d55b23ae9
smhasanjamil <smhasanjamil23@gmail.com>
2023-06-04 16:14:05 +0600

    Category added to home

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,13 +6,14 @@ import img3 from '../../../assets/home/03.png';
 import img4 from '../../../assets/home/04.jpg';
 import img5 from '../../../assets/home/05.png';
 import img6 from '../../../assets/home/06.png';
+import './Banner.css'
 
 const Banner = () => {
 
     return (
         <>
 
-            <Carousel>
+            <Carousel autoPlay={true} infiniteLoop={true}>
                 <div>
                     <img src={img1} />
                 </div>
