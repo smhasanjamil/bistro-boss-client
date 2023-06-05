b1dfd988826be601d3a771b900e6c730fa5809b8
smhasanjamil <smhasanjamil23@gmail.com>
2023-06-05 06:48:52 +0600

    Popular menu added

---
+++ src/pages/Shared/MenuItems/MenuItems.jsx
@@ -0,0 +1,17 @@
+
+
+const MenuItems = ({ item }) => {
+    const { name, image, price, recipe } = item;
+    return (
+        <div className="flex space-x-4">
+            <img className="w-[100px]" src={image} alt="menu image" style={{borderRadius:' 0 200px 200px 200px'}} />
+            <div>
+                <h3 className="uppercase">{name}----------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className=" text-yellow-500">{price}</p>
+        </div>
+    );
+};
+
+export default MenuItems;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -3,17 +3,9 @@
 const Navbar = () => {
 
     const navOptions = <>
-       
+
         <li><a>Item 1</a></li>
-        <li tabIndex={0}>
-            <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-            </details>
-        </li>
+        <li><a>Item 2</a></li>
         <li><a>Item 3</a></li>
 
     </>
@@ -29,7 +21,7 @@ const Navbar = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-black bg-opacity-30 rounded-box w-52">
                             {/* <li><a>Item 1</a></li>
                             <li>
                                 <a>Parent</a>
